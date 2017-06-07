93bc85bfe23814e9ad30b013fedb659578234e34
pavellint <work@1int.org>
2017-06-07 13:51:49 +0600

    fix

---
+++ app/Models/Shopify/Shipment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Shopify;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shipment extends Model
+{
+    protected $table = 'shopify_shipments';
+}
---
+++ app/Models/Shopify/Shop.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Shopify;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    protected $table = 'shopify_shops';
+}
