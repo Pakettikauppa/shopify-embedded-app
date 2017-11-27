07d936265e615455e38afb8d52860c960c7cadec
Joose Vettenranta <joose.vettenranta@kompassi.fi>
2017-11-27 14:40:02 +0200

    Force shipping methods codes as strings

--- app/Http/Controllers/Shopify/AppController.php
+++ app/Http/Controllers/Shopify/AppController.php
@@ -176,7 +176,7 @@ class AppController extends Controller
 
         $productProviderByCode = array();
         foreach($products as $_product) {
-            $productProviderByCode[$_product['shipping_method_code']] = $_product['service_provider'];
+            $productProviderByCode[(string) $_product['shipping_method_code']] = $_product['service_provider'];
         }
 
         $shipping_settings = [];
@@ -184,7 +184,7 @@ class AppController extends Controller
             $shipping_settings[] = [
                 'shipping_rate_id' => $key,
                 'product_code' => $code,
-                'service_provider' => $productProviderByCode[$code]
+                'service_provider' => $productProviderByCode[(string) $code]
              ];
         }
 
