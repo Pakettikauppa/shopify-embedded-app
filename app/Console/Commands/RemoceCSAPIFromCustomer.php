7cddb3f8bdad3fddbf44a11b18d34ac3e85fac53
Joose Vettenranta <joose.vettenranta@kompassi.fi>
2019-01-16 11:56:51 +0200

    New command to remove Custom Carrier Service

---
+++ app/Console/Commands/RemoceCSAPIFromCustomer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Shopify\Shop;
+use App\Models\Shopify\ShopifyClient;
+use Illuminate\Console\Command;
+
+class RemoceCSAPIFromCustomer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'shopify:remove-cs-api {shop_origin}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove Custome Carrier Service API from a customer';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $shop = Shop::where('shop_origin', $this->argument('shop_origin'))->first();
+
+        if(empty($shop)) {
+            echo "No such shop_origin\n";
+            return;
+        }
+
+        $_client = new ShopifyClient($shop->shop_origin, $shop->token, ENV('SHOPIFY_API_KEY'), ENV('SHOPIFY_SECRET'));
+
+        try {
+            $_client->call('DELETE', '/admin/carrier_services/' . $shop->carrier_service_id . '.json');
+
+            $shop->carrier_service_id = null;
+            $shop->save();
+        } catch(\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
