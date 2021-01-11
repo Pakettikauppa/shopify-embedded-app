f5fa93139183a2abc28a8bab84369a03e35c6aef
Joose Vettenranta <joose.vettenranta@pakettikauppa.fi>
2021-01-11 10:38:29 +0200

    Fix for print label bug

--- app/Http/Controllers/Shopify/AppController.php
+++ app/Http/Controllers/Shopify/AppController.php
@@ -171,7 +171,7 @@ class AppController extends Controller
             $shipment['admin_order_url'] = 'https://' . $shop->shop_origin . '/admin/orders/' . $order['id'];
             $url_params = [
                 'shop' => $shop->shop_origin,
-                'is_return' => $is_return,
+                'is_return' => $is_return?'1':'0',
             ];
 
             $url_params['hmac'] = createShopifyHMAC($url_params);
