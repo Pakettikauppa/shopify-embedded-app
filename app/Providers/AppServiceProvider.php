7201bf4a2714943279eb0d36b551559df67dafff
unknown <pampyras@gmail.com>
2021-04-13 20:01:09 +0300

    Added URL::forceRootUrl to generate route url from env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         URL::forceScheme('https');
+        URL::forceRootUrl(config('app.url'));
     }
 
     /**
