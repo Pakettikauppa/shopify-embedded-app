813159dd6a2c9c25b7fe46323f1ae1a3d3e9d2a0
Joose Vettenranta <joose@kompassi.fi>
2022-10-10 22:33:41 +0300

    Remove debug data

--- resources/views/settings/pickuppoints.blade.php
+++ resources/views/settings/pickuppoints.blade.php
@@ -5,7 +5,6 @@
 
     <article>
         <div class="card">
-            <!-- pickup points 2 -->
             <h2>{{trans('app.settings.pickuppoints.title')}}</h2>
             @if ($shop->carrier_service_id == null)
             {{trans('app.settings.enable_carrier_api')}}
@@ -28,11 +27,6 @@
                                </th>
                            </tr>
                             @foreach($shipping_methods as $shipping_method)
-                                <!--
-                                @php
-                                echo json_encode($shipping_method);
-                                @endphp
-                                -->
                                 @if ($shipping_method['has_pickup_points'])
                                     @php
                                         $shippingMethodCode = (string) $shipping_method['shipping_method_code'];
