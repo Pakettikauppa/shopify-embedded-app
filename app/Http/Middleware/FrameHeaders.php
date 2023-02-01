0c3e3b1eb4a2c8f7918ad897e372e94f9e46a7ec
Joose Vettenranta <joose@kompassi.fi>
2023-02-01 11:44:28 +0200

    revert to original csp

--- app/Http/Middleware/FrameHeaders.php
+++ app/Http/Middleware/FrameHeaders.php
@@ -18,8 +18,8 @@ class FrameHeaders
     {
         $shop_domain = $request->get('shop');
         $response = $next($request);
-//        $response->header('Content-Security-Policy', "frame-ancestors https://{$shop_domain} https://admin.shopify.com;");
-        $response->header('Content-Security-Policy', "default-src 'self'; script-src 'self' cdn.shopify.com cdn.shopifycloud.com; style-src 'self' cdn.shopify.com cdn.shopifycloud.com 'unsafe-inline'; img-src 'self' cdn.shopify.com cdn.shopifycloud.com v.shopify.com data:; font-src 'self' cdn.shopify.com cdn.shopifycloud.com data:; frame-ancestors https://{$shop_domain} https://admin.shopify.com; upgrade-insecure-requests");
+        $response->header('Content-Security-Policy', "frame-ancestors https://{$shop_domain} https://admin.shopify.com;");
+//        $response->header('Content-Security-Policy', "default-src 'self'; script-src 'self' cdn.shopify.com cdn.shopifycloud.com; style-src 'self' cdn.shopify.com cdn.shopifycloud.com 'unsafe-inline'; img-src 'self' cdn.shopify.com cdn.shopifycloud.com v.shopify.com data:; font-src 'self' cdn.shopify.com cdn.shopifycloud.com data:; frame-ancestors https://{$shop_domain} https://admin.shopify.com; upgrade-insecure-requests");
         return $response;
     }
 }
