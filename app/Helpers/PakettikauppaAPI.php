9a51e56c6ca2bc1c391a3273e11c38e4211f5d26
Vilius Dargevičius <vilius@vkodas.lt>
2024-03-27 11:40:23 +0200

    Cleanup

--- app/Helpers/PakettikauppaAPI.php
+++ app/Helpers/PakettikauppaAPI.php
@@ -5,34 +5,8 @@ namespace App\Helpers;
 use Illuminate\Support\Facades\Log;
 use Pakettikauppa\Client;
 
-/**
- * @property $http_request
- * @property $http_response_code
- * @property $http_error
- * @property $http_response
- */
 class PakettikauppaAPI extends Client
 {
-//    public function __construct(array $configs = null, $use_config = null)
-//    {
-//        p
-//    }
-//
-//    public function setSenderSystemName(string $sender_system_name): void
-//    {
-//        $this->client->setSenderSystemName($sender_system_name);
-//    }
-//
-//    public function getToken(): mixed
-//    {
-//        return $this->client->getToken();
-//    }
-//
-//    public function setAccessToken($access_token): void
-//    {
-//        $this->client->setAccessToken($access_token);
-//    }
-
     public function searchPickupPoints($postcode = null, $street_address = null, $country = null, $service_provider = null, $limit = 5, $type = null, $attempts = 1): array|null
     {
         for ($i=1; $i<=$attempts; $i++) {
